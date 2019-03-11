34d0ed1955f334a982141228e202d94bf2d827f5
ahmet <kilichaciahmet@gmail.com>
2019-03-11 09:58:06 +0100

    kurs ilk bölüm bitti

--- MvcMosh1/Controllers/MoviesController.cs
+++ MvcMosh1/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcMosh1.Models;
+using MvcMosh1.ViewModels;
 
 namespace MvcMosh1.Controllers
 {
@@ -13,8 +14,18 @@ namespace MvcMosh1.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie=movie,
+                Customers=customers
+            };
+            return View(viewModel);
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
@@ -38,6 +49,11 @@ namespace MvcMosh1.Controllers
             }
             return Content(String.Format("pageIndex = {0}&SortBy = {1}", pageIndex, SortBy));
 
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,2)}")]
+       public ActionResult ByReleaseDate(int year , int month)
+        {
+            return Content(year + " / " + month );
         }
     }
 }
